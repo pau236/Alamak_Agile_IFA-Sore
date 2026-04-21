4c2b614c0e1c370c763924c1924491024ecc92a8
pau236 <paulineangelicca005@gmail.com>
2026-04-21 13:14:19 +0700

    Memperbarui koneksi database menggunakan environment variables

--- database/connect_db.js
+++ database/connect_db.js
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/FoodRescue");
-    console.log("Database Connected Successfully!");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ Database Connected Successfully!");
   } catch (err) {
-    console.log(err);
+    console.error("❌ Database Connection Failed:", err.message);
+    process.exit(1);
   }
 };
 
